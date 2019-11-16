7bd9f46635559076624740f9cab134bb74c1c208
Ramón Ventura <50932514+Ramon-Ventura@users.noreply.github.com>
2019-11-16 10:30:44 -0600

    añadiendo fragments

--- app/src/main/java/com/example/spaceapp/MainActivity.kt
+++ app/src/main/java/com/example/spaceapp/MainActivity.kt
@@ -4,27 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.fragment.app.FragmentTransaction
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.spaceapp.api_apod.ApiResponseApod
+//import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_main.*
+
+import kotlinx.android.synthetic.main.fragment_apod.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var apodFragment: ApodFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        verifyAndConnect()
+        verifyAndConnect("https://api.nasa.gov/planetary/apod?api_key=mbXbdV3EpYydttCB6qbySduGMtYiS3TNyb0JagqH")
+
+        //val bottomNavigation: BottomNavigationView = findViewById(R.id.btm_nav)
+        this.apodFragment = ApodFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout,apodFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
     }
 
     //Verify an existing connection
-    private fun verifyAndConnect(){
+    fun verifyAndConnect(url:String = "oim"){
         if(Network.verifyConection(this)){
-            httpVolley(getUrlApi())
+            httpVolley(url)
         }else{
             Toast.makeText(this,"No internet connection", Toast.LENGTH_SHORT).show()
         }
@@ -47,24 +60,19 @@ class MainActivity : AppCompatActivity() {
         queue.add(stringRequest)
     }
 
-    private fun getUrlApi (): String{
-        return "https://api.nasa.gov/planetary/apod?api_key=mbXbdV3EpYydttCB6qbySduGMtYiS3TNyb0JagqH"
-    }
-
     private fun jsonToObject(response: String){
         val gson= Gson()
         val apiResponse = gson.fromJson(response, ApiResponseApod::class.java)
         val urlImagen = apiResponse.hdurl
-        textViewTitle.text=apiResponse.title
-        textViewExplanation.text = apiResponse.explanation
-        textViewCopy.text=apiResponse.copyright
-        textViewDate.text=apiResponse.date
+        apodFragment.textViewTitle.text=apiResponse.title
+        apodFragment.textViewExplanation.text = apiResponse.explanation
+        apodFragment.textViewCopy.text=apiResponse.copyright
+        apodFragment.textViewDate.text=apiResponse.date
 
         Picasso.get()
             .load(urlImagen)
             .resize(500,500)
-            .into(imageViewApod)
-
+            .into(apodFragment.imageViewApod)
 
         Toast.makeText(this, "Data Success", Toast.LENGTH_SHORT).show()
     }
